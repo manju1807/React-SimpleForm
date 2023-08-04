fcaa3338d97a24bf1419016a71d37e6b0543d5c8
manju1807 <rmanjunath7692@gmail.com>
2023-08-04 16:02:12 +0530

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,13 +23,7 @@ const Home = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (
-      !inputs.name ||
-      !inputs.email ||
-      !inputs.gender ||
-      !inputs.dob ||
-      !inputs.phone
-    ) {
+    if (!Object.values(inputs).every(Boolean)) {
       alert("All fields are required.");
       return;
     }
@@ -60,20 +54,14 @@ const Home = () => {
 
   const handleEdit = (index) => {
     const tempData = tableData[index];
-    setInputs({
-      name: tempData.name,
-      email: tempData.email,
-      gender: tempData.gender,
-      dob: tempData.dob,
-      phone: tempData.phone,
-    });
+    setInputs(tempData);
     setEditClick(true);
     setEditIndex(index);
   };
 
   return (
     <div className="bg-gradient-to-t from-[#ff9efa] to-[#b988fd] min-h-screen flex items-center justify-center rounded-[5px]">
-      <div className="w-[700px] overflow-hidden bg-transparent rounded-[5px]">
+      <div className="w-[700px] h-auto overflow-hidden bg-transparent rounded-[5px]">
         <div className="rounded-[5px]">
           <div className="flex rounded-[5px]">
             <div className="w-1/2 bg-black">
@@ -87,71 +75,56 @@ const Home = () => {
               <h1 className="text-2xl text-center font-sans italic font-bold">
                 Register Now!
               </h1>
-              <form onSubmit={handleSubmit} className="mt-4">
-                <label htmlFor="name" className="block font-bold mb-1">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  value={inputs.name}
-                  onChange={handleChange}
-                  placeholder="Enter your Name"
-                  className="input-style text-[#383838] italic p-1 rounded-sm"
-                />
-                <label htmlFor="email" className="block font-bold mb-1 mt-2">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  value={inputs.email}
-                  onChange={handleChange}
-                  placeholder="Enter your Email"
-                  className="input-style text-[#383838] italic p-1 rounded-sm"
-                />
-                <label htmlFor="gender" className="block font-bold mb-1 mt-2">
-                  Gender
-                </label>
-                <select
-                  name="gender"
-                  value={inputs.gender}
-                  onChange={handleChange}
-                  className="input-style text-[#6a6a6a] px-3 py-1 rounded border italic bg-white border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
-                >
-                  <option value="male">Male</option>
-                  <option value="female">Female</option>
-                  <option value="others">Others</option>
-                </select>
-
-                <label htmlFor="dob" className="block font-bold mb-1 mt-2">
-                  Date of Birth
-                </label>
-                <input
-                  type="date"
-                  name="dob"
-                  value={inputs.dob}
-                  onChange={handleChange}
-                  placeholder="date of birth"
-                  className="input-style text-[#515151] italic p-1 rounded-sm"
-                />
-                <label htmlFor="phone" className="block font-bold mb-1 mt-2">
-                  Phone
-                </label>
-                <input
-                  type="text"
-                  name="phone"
-                  value={inputs.phone}
-                  onChange={handleChange}
-                  placeholder="phone number"
-                  className="input-style text-[#383838] italic p-1 rounded-sm"
-                />
-                <button
-                  type="submit"
-                  className="mt-4 px-4 py-2 rounded-sm bg-green-500 text-white w-full"
-                >
-                  {editClick ? "Update" : "Submit"}
-                </button>
+              <form onSubmit={handleSubmit} className="mt-8 ">
+                {[
+                  { label: "Name", name: "name" },
+                  { label: "Email", name: "email" },
+                  {
+                    label: "Gender",
+                    name: "gender",
+                    type: "select",
+                    options: ["Male", "Female", "Others"],
+                  },
+                  { label: "Date of Birth", name: "dob", type: "date" },
+                  { label: "Phone", name: "phone" },
+                ].map(({ label, name, type, options }) => (
+                  <div key={name} className="mb-2">
+                    <label htmlFor={name} className="block font-bold mb-1">
+                      {label}
+                    </label>
+                    {type === "select" ? (
+                      <select
+                        name={name}
+                        value={inputs[name]}
+                        onChange={handleChange}
+                        className="appearance-none italic bg-transparent border-none w-fit text-white mr-3 py-1 px-2 leading-tight focus:outline-none"
+                      >
+                        {options.map((option) => (
+                          <option key={option} value={option.toLowerCase()}>
+                            {option}
+                          </option>
+                        ))}
+                      </select>
+                    ) : (
+                      <input
+                        type={type || "text"}
+                        name={name}
+                        value={inputs[name]}
+                        onChange={handleChange}
+                        placeholder={`Enter your ${label}`}
+                        className="appearance-none italic bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none"
+                      />
+                    )}
+                  </div>
+                ))}
+                <div className="mt-3 grid">
+                  <button
+                    type="submit"
+                    className="mt-6 px-4 py-2 rounded-sm bg-green-500 font-semibold text-white"
+                  >
+                    {editClick ? "Update" : "Submit"}
+                  </button>
+                </div>
               </form>
             </div>
           </div>
